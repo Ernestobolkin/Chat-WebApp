335cb03cefeb2f4c6205e79f375b1c3247b9eb4b
Ernest-Tobolkin <ernest1047@gmail.com>
2023-07-14 16:29:15 +0300

    user schema added

---
+++ Chat-App BE/src/models/user.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    passwordHash: {
+        type: String,
+    },
+    // avatar: {
+    //     type: String,
+    // },
+    // friends: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'User'
+    // }],
+    // friendRequests: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'User'
+    // }],
+    // sentFriendRequests: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'User'
+    // }],
+    // conversations: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Conversation'
+    // }],
+    // socketId: {
+    //     type: String,
+    // },
+    // status: {
+    //     type: String,
+    //     enum: ['online', 'offline', 'away'],
+    //     default: 'offline'
+    // },
+    // lastSeen: {
+    //     type: Date,
+    //     default: Date.now
+    // },
+    // isDeleted: {
+    //     type: Boolean,
+    //     default: false
+    // }
+}, { timestamps: true });
+
+
+export const User = mongoose.model('User', userSchema);
