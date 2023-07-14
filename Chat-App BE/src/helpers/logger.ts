d7f8fff3f946ad56c47a8b70bb627a232bac2862
Ernest-Tobolkin <ernest1047@gmail.com>
2023-07-14 16:28:52 +0300

    logger fix

--- Chat-App BE/src/helpers/logger.ts
+++ Chat-App BE/src/helpers/logger.ts
@@ -8,11 +8,13 @@ export const requestLogger = (req: Request, res: Response, next: NextFunction) =
     next();
 }
 
-export const  responseLogger = (req: Request, res: Response, next: NextFunction) => {
-    if(res.statusCode >= 400) {
+export const responseLogger = (req: Request, res: Response, next: NextFunction) => {
+    res.on('finish', () => {
+      if (res.statusCode >= 400) {
         console.log(chalk.red(req.method, req.originalUrl, res.statusCode));
-        return next();
-    }
-    console.log(req.method, req.originalUrl, chalk.green(res.statusCode));
+      } else {
+        console.log(req.method, req.originalUrl, chalk.green(res.statusCode));
+      }
+    });
     next();
-}
+  };
