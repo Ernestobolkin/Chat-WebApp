5d9744f4b4051274f6f7264eae8b0fe29a97594a
Ernest-Tobolkin <ernest1047@gmail.com>
2023-10-22 00:08:54 +0300

    git ignore

--- Chat-App-BE/src/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-    PORT: process.env.PORT || 3000,
-    MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING || 'mongodb+srv://elzo:XhxnvNubdu@ertodatabase.ilvau.mongodb.net/?retryWrites=true&w=majority',
-    PASSWORD_REGEX: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/,
-    EMAIL_REGEX:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-}
