81c78129615ef7dcfb71ed6e08cb508c14df2c01
ternest <ernest1047@gmail.com>
2023-11-14 00:15:52 +0200

    post card style added

--- Chat-App-FE/src/components/posts/post.tsx
+++ Chat-App-FE/src/components/posts/post.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import usePostStore from '../../stores/postStore';
 import PostCreationComponent from './post-create';
 import { generalRequest } from '../../service/api-service';
-
+import "./post.style.css";
 
 const PostListComponent: React.FC = () => {
     const posts = usePostStore((state: any) => state.posts);
@@ -11,7 +11,7 @@ const PostListComponent: React.FC = () => {
 
     useEffect(() => {
         fetchPosts();
-    },[])
+    }, [])
 
 
     const fetchPosts = async () => {
@@ -25,13 +25,45 @@ const PostListComponent: React.FC = () => {
                 <PostCreationComponent />
             </div>
             <h2>Posts</h2>
-            <ul>
+            <div className="row">
                 {posts.map((post, index) => (
-                    <li key={index}>{post.textContent}</li>
+                    <div className="col-md-4 mb-4" key={index}>
+                        <div className="card" style={{ width: '18rem' }}>
+                            <div className="card-header">
+                                <h5 className="card-title">
+                                    <a href="#" className="font-weight-bold">
+                                        {post?.author?.username}
+                                    </a>
+                                    <div className="user-icon"></div>
+                                </h5>
+                            </div>
+                            {post?.image && (
+                                <div className="details-container">
+                                    {post.imageUrl && (
+                                        <img src={post?.imageUrl} className="card-img-top" alt="User Post" />
+                                    )}
+                                </div>
+                            )}
+                            <div className="card-body">
+                                <div className="details-container">
+                                    <p className="card-text">{post.textContent}</p>
+                                </div>
+                                {/* Add comment count and link */}
+                            </div>
+                            <div className="card-footer">
+                            <a href="" className="comments-link">
+                                    {post.commentsCount} Comments 
+                                </a>
+                                <button className="btn btn-primary">Like 
+                                <span className="badge badge-light">{post.likes.length}</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 ))}
-            </ul>
+            </div>
         </div>
     );
-}
+};
 
 export default PostListComponent;
